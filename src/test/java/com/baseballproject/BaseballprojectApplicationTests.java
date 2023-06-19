fecfcc06773b107c730f190922e34912683c4daa
hamsuri <yebear96@gmail.com>
2023-06-19 11:12:54 +0900

    project

---
+++ src/test/java/com/baseballproject/BaseballprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baseballproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaseballprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
