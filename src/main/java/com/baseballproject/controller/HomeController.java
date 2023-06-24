155374bae9b3d950f99a5581ecab9484f48c2813
kimsuhhhh <121539156+kimsuhhhh@users.noreply.github.com>
2023-06-24 17:53:09 +0900

    kimsu

--- src/main/java/com/baseballproject/controller/HomeController.java
+++ src/main/java/com/baseballproject/controller/HomeController.java
@@ -11,118 +11,104 @@ public class HomeController {
 
         return "index";
     }
-<<<<<<< HEAD
+
+    // 두산 매핑 시작
 
     @GetMapping(value = "/bears")
     public String bears() {
 
-        return "index_bear";
-    }
-
-    @GetMapping(value = "/twins")
-    public String twins() {
-
-        return "index_twin";
-=======
-    
-    //두산 매핑 시작
-
-    @GetMapping(value = "/bears")
-    public String bears(){
-      
         return "./bears/index_bear";
     }
 
     @GetMapping(value = "/bears/cheer")
-    public String bears_cheer(){
-      
+    public String bears_cheer() {
+
         return "./bears/cheer_bear";
     }
 
     @GetMapping(value = "/bears/info_play")
-    public String bears_info_play(){
-      
+    public String bears_info_play() {
+
         return "./bears/info_play_bear";
     }
 
     @GetMapping(value = "/bears/info_py")
-    public String bears_info_py(){
-      
+    public String bears_info_py() {
+
         return "./bears/info_py_bear";
     }
 
     @GetMapping(value = "/bears/new/book")
-    public String bears_book(){
-      
+    public String bears_book() {
+
         return "./bears/new_baseball_book_bear";
     }
 
     @GetMapping(value = "/bears/new/cheer")
-    public String bears_cheer_new(){
-      
+    public String bears_cheer_new() {
+
         return "./bears/new_baseball_cheer_bear";
     }
 
     @GetMapping(value = "/bears/new/rule")
-    public String bears_rule(){
-      
+    public String bears_rule() {
+
         return "./bears/new_baseball_rule_bear";
-    }   
+    }
 
     @GetMapping(value = "/bears/new/words")
-    public String bears_words(){
-      
+    public String bears_words() {
+
         return "./bears/new_baseball_words_bear";
     }
 
-    //LG 매핑 시작
+    // LG 매핑 시작
 
     @GetMapping(value = "/twins")
-    public String twins(){
-      
+    public String twins() {
+
         return "./twins/index_twin";
     }
 
     @GetMapping(value = "/twins/cheer")
-    public String twins_cheer(){
-      
+    public String twins_cheer() {
+
         return "./twins/cheer_twins";
     }
 
     @GetMapping(value = "/twins/info_play")
-    public String twins_info_play(){
-      
+    public String twins_info_play() {
+
         return "./twins/info_play_twins";
     }
 
     @GetMapping(value = "/twins/info_py")
-    public String twins_info_py(){
-      
+    public String twins_info_py() {
+
         return "./twins/info_py_twins";
     }
 
     @GetMapping(value = "/twins/new/book")
-    public String twins_book(){
-      
+    public String twins_book() {
+
         return "./twins/new_baseball_book_twins";
     }
 
     @GetMapping(value = "/twins/new/cheer")
-    public String twins_cheer_new(){
-      
+    public String twins_cheer_new() {
+
         return "./twins/new_baseball_cheer_twins";
     }
 
     @GetMapping(value = "/twins/new/rule")
-    public String twins_rule(){
-      
+    public String twins_rule() {
+
         return "./twins/new_baseball_rule_twins";
-    }   
+    }
 
     @GetMapping(value = "/twins/new/words")
-    public String twins_words(){
-      
+    public String twins_words() {
+
         return "./twins/new_baseball_words_twins";
->>>>>>> 12d6c43b6b54fd3e51c7244155fb3c37f343c6ba
     }
 }
