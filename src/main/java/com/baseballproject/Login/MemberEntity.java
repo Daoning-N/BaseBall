f71be6eb4b2232e2bf56835820d0112c832b158d
daon <daoning23@naver.com>
2023-07-06 22:38:32 +0900

    로그인

---
+++ src/main/java/com/baseballproject/Login/MemberEntity.java
@@ -0,0 +1,42 @@
+package com.baseballproject.Login;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Setter
+@Getter
+@Table(name = "member_table") 
+public class MemberEntity { 
+    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) 
+    private Long id;
+
+    @Column(unique = true)
+    private String memberEmail;
+
+    @Column
+    private String memberPassword;
+
+    @Column
+    private String memberName;
+
+    public static MemberEntity toMemberEntity(MemberDTO memberDTO){
+        MemberEntity memberEntity = new MemberEntity();
+        memberEntity.setId(memberDTO.getId());
+        memberEntity.setMemberEmail(memberDTO.getMemberEmail());
+        memberEntity.setMemberName(memberDTO.getMemberName());
+        memberEntity.setMemberPassword(memberDTO.getMemberPassword());
+        return memberEntity;
+    }
+
+}
